a9ad9c0a241dbfff422b705d94f1d3c6a5004a69
Rakker199 <68775102+Rakker199@users.noreply.github.com>
2022-10-29 16:54:21 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
-public class Main {
+package com.practice;
+
+public class Main
+{
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Testing..");
+        MyWindow w=new MyWindow();
     }
 }
